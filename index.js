9b8401bfc8468f0b8925c6d4a35e655495fc409e
milan saluja <iammilansaluja05@gmailcom>
2024-04-04 17:37:28 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,11 @@
+document.getElementById("hamburger").addEventListener("click",()=>{
+document.getElementById("ul");
+ul.style.display="block";
+ul.style.top="100%";
+ul.style.right="0%";
+});
+
+document.querySelector("#ul").getElementsByTagName("img")[0].addEventListener("click",()=>{
+    ul.style.top= "-430%";
+    ul.style.right="-20%";
+});
